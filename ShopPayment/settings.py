4043fcc60318d0ec2d599d305444e6b2f8b932c5
Illia Salohub <eramcheg@gmail.com>
2025-02-17 21:34:48 +0100

    Update: * Added text and html version of email

--- ShopPayment/settings.py
+++ ShopPayment/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
